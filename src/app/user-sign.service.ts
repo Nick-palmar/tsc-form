cd8809b9f7f906231503abcd334e455915c259f0
Nick-palmar <nicolas.palmar8@gmail.com>
2020-08-14 19:07:09 -0400

    new post body and bug fixes

--- src/app/user-sign.service.ts
+++ src/app/user-sign.service.ts
@@ -12,8 +12,24 @@ export class UserSignService {
 		return this._appUrl;
 	}
 	signup(date, name, email, role, age, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10) {
-		const url = this.getUrl() + "?formDate=" + date + "&name=" + name + "&email=" + email + "&role=" + role + "&age=" + age + "&question1=" + q1 + "&question2=" + q2 + "&question3=" + q3 + "&question4=" + q4 + "&question5=" + q5 + "&question6=" + q6 + "&question7=" + q7 + "&question8=" + q8 + "&question9=" + q9 + "&question10=" + q10; 
+		const url = this.getUrl(); 
 		console.log(url);
-		return this._http.post<any>(url, name);
+		return this._http.post<any>(url, {
+			"formDate": date,
+			"name": name, 
+			"email": email,
+			"role": role,
+			"age": age,
+			"question1": q1, 
+			"question2": q2,
+			"question3": q3,
+			"question4": q4,
+			"question5": q5,
+			"question6": q6,
+			"question7": q7,
+			"question8": q8,
+			"question9": q9,
+			"question10": q10
+		});
 	}
 }
